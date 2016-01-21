ec884784eecb3d504af5693172ec2bd0f460e1af
john-donut <user@localhost.localdomain>
2016-01-21 19:49:13 +0000

    mais pi c'est 3.14 !

--- source/constants.f90
+++ source/constants.f90
@@ -1,10 +1,7 @@
 module constants
     integer:: dim=2     !pour commencer
-<<<<<<< HEAD
-    real(8):: pi=3.05
-=======
     real(8):: pi=3.1415	!manque de précision
->>>>>>> 39d10cd3f52cb69ce81d97086e6f5ed0e8f7c7d3
+
 
 end module
 
