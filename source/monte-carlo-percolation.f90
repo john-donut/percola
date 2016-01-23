50aadf1d641f95d438879feb8bec923988645872
john-donut <jdzr@m.ddcrew.com>
2016-01-23 19:49:11 +0100

    traduction en fortran du programme en C proposé dans l'article. Compile pas encore

---
+++ source/monte-carlo-percolation.f90
@@ -0,0 +1,96 @@
+! Algorithm inspired from M. E. J. Newman & R. M. Ziff 2001 paper. It
+! calculates, with algorithmic complexity O(n) wether or not there is
+! percolation.
+
+!The version of the program we have given in Ap-
+!pendix A measures the largest cluster size as a function
+!of number of occupied sites.
+
+!Below is a simple translation of the functions in fortran
+!In this appendix we give a complete program in C for our algorithm for site percolation on a square lattice of N = L × L sites with periodic boundary conditions. This program prints out the size of the largest cluster on the lattice as a function of number of occupied sites n for values of n from 1 to N . The entire program consists of 73 lines of code. First we set up some constants and global variables:
+
+module constants_mcp
+    integer, parameter :: L=128   !Linear dimension
+    integer :: N=L*L
+    integer :: EMPTY=(-N-1) !?
+    integer, dimension(N) :: ptr, order   !Array of pointers, Nearest neighbors
+    integer, dimension(N,4) :: pnn      !Occupation order
+end module
+
+subroutine  boundaries()
+    !Next we set up the array nn[][] which contains a list of the nearest neighbors of each site. Only this array need be changed in order for the program to work with a lattice of different topology.
+    use constants_mcp
+    integer i
+    do i=0, N
+    nn[i][0] = (i+1)%N;
+    nn[i][1] = (i+N-1)%N;
+    nn[i][2] = (i+L)%N;
+    nn[i][3] = (i+N-L)%N;
+    if (i%L==0) then
+        nn[i][1] = i+L-1
+    endif
+    if ((i+1)%L==0) then
+        nn[i][0] = i-L+1 
+    endif
+    enddo
+end subroutine
+
+subroutine permutation
+    !Now we generate the random order in which the sites will be occupied, by randomly permuting the integers from 0 to N − 1:
+    integer :: i, j, temp
+    do i=0, N
+        order[i] = i;
+    enddo
+    do i=0, N
+        j = i + (N-i)*drand();
+    !Here the function drand() generates a random double precision floating point number between 0 and 1.
+        temp = order[i];
+        order[i] = order[j];
+        order[j] = temp;
+    enddo
+end subroutine
+
+function findroot(i)
+use constants_mcp
+    integer, intent(in) :: i
+    if (ptr[i]<0) then
+        return i
+    return ptr[i] = findroot(ptr[i])
+end function
+
+subroutine percolate
+    integer :: i,j,s1,s2,r1,r2,big=0
+    do i=0, N
+        ptr[i] = EMPTY
+    enddo
+    do i=0, N
+        r1 = s1 = order[i]
+        ptr[s1] = -1
+        do j=0, 4
+            s2 = nn[s1][j]
+            if (ptr[s2]<>EMPTY) then
+                r2 = findroot(s2)
+                if (r2<>r1) then
+                    if (ptr[r1]>ptr[r2]) then
+                        ptr[r2] += ptr[r1]
+                        ptr[r1] = r2
+                        r1 = r2
+                    else
+                        ptr[r1] += ptr[r2]
+                        ptr[r2] = r1
+                    endif
+                    if (-ptr[r1]>big) then
+                        big = -ptr[r1]
+                    endif
+                endif
+            endif
+        write(*,*), i, i, i+1, big
+        enddo
+    enddo
+end subroutine
+
+program main
+    call boundaries
+    call permutation
+    call percolate
+end program
