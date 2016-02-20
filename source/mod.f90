c5e367dd8c5b81fdeec6e729c8184cff71dcf905
john-donut <jdzr@m.ddcrew.com>
2016-02-20 18:31:42 +0000

    Delete mod.f90
    Cleanup

--- source/mod.f90
+++
@@ -1,7 +0,0 @@
-module constante
-
-	integer, parameter :: taille_reseau = 20, dimension_reseau = 2
-	logical, dimension(taille_reseau, taille_reseau) :: reseau
-	real(8) :: proba
-    
-end module constante
