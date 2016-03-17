fb2154d4acbeed10919380ba1e4f233a699fae5b
dodo27410 <luis.linan14@gmail.com>
2016-03-17 09:12:40 +0100

    Update compile.sh

--- source/compile.sh
+++ source/compile.sh
@@ -1,2 +1,2 @@
 gfortran -c random_functions.f90 -o random_functions.o
-gfortran -fbounds-check -o program.x monte-carlo-percolation.f90 random_functions.o
+gfortran -fbounds-check -o program.x susc.f90 random_functions.o
