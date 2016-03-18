513bf0bc06c9e1b3a11cbb7a9e3d0135769f1c77
john-donut <jdzr@m.ddcrew.com>
2016-03-18 01:09:57 +0100

    cleanup

--- source/compile.sh
+++ source/compile.sh
@@ -1,2 +1,3 @@
 gfortran -c random_functions.f90 -o random_functions.o
-gfortran -fbounds-check -o program.x monte-carlo-percolation.f90 random_functions.o
+gfortran -fbounds-check -o program.x susc.f90 random_functions.o
+#gfortran -fbounds-check -o program.x monte-carlo-percolation.f90 random_functions.o
